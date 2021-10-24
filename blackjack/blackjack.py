a4b2d1289b77d478c3f2cf55ef9ae49bdc65b021
QuackZ90 <82066443+QuackZ90@users.noreply.github.com>
2021-10-24 12:28:01 +0800

    Updated with docstring

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -8,6 +8,7 @@ class Deck():
   suits = ["♢", "♣", "♡", "♠",  ]
 
   def __init__(self):
+    """Initiate a new deck of 52 cards"""
     self.deck = []
     for suit in self.suits:
       for card in self.cards:
@@ -21,6 +22,7 @@ class Deck():
 class Hand():
 
   def __init__(self, currdeck):
+    """Initiate a new hand by drawing 2 cards from the currdeck, then update the value of the hand"""
     self.hand=[]
     self.draw(currdeck)
     self.draw(currdeck)
@@ -36,12 +38,17 @@ class Hand():
     return self.value == other.value
 
   def draw (self, currdeck):
+    """Draw a card from currdeck to the player's hand and update the value of the hand"""
     card = currdeck.deck.pop(random.randint(0,len(currdeck.deck)-1))
     self.hand.append(card)
     self.value = self.checkValue()
     return card
 
   def checkValue(self):
+    """Calculate and return the value of the cards in the hand.
+    Number cards has the value of its number.
+    Picture cards has a value of 10.
+    Ace has a value of 11 if the total value is less than 21, otherwise, it has a value of 1"""
     value = 0
     numA = 0
 
@@ -79,25 +86,27 @@ while new_game == "y":
   print(f"Computer drew: {ai_hand.hand[0]}.")
 
   if player_hand.value == 21:
+    """Check for blackjack"""
     print(f"Computer's hand: {ai_hand}.")
-    if ai_hand.value ==21:
-      print("Both hand drew 21. It is a draw")
+    if ai_hand.value == 21:
+      print("Both hand drew a blackjack. It is a draw")
       new_game=input("Would you like to play again?(Y/N)").lower()
       continue
     else:
-      print("You drew 21. You won!")
+      print("You drew a blackblack. You won!")
       new_game=input("Would you like to play again?(Y/N)").lower()
       continue
 
   elif ai_hand.value ==21:
     print(f"Computer's hand: {ai_hand}.")
-    print("Computer drew 21. You lost")
+    print("Computer drew a backjack. You lost")
     new_game=input("Would you like to play again?(Y/N)").lower()
     continue
 
   new_card = input("Would you like to draw another card?(Y/N)").lower()
 
   while new_card =="y" and player_hand.value < 21:
+    """player gets to decide whether to draw a card as long as the value of card in hand is less then 21"""
     card_drawn = player_hand.draw(current_deck)
     print(f"You drew: {card_drawn}")
     print(f"Your current hand:{player_hand}. Hand's value: {player_hand.value}")
@@ -108,6 +117,7 @@ while new_game == "y":
     new_card = input("Would you like to draw another card?(Y/N)").lower()
 
   while (ai_hand.value < 16 or (ai_hand < player_hand and player_hand.value <= 21)):
+    """computer draws a card as long as the value of card in hand is less then 16, or if the value of card in hand is less than player's when player's card is not more than 21"""
     print("Computer is deciding on it's next move", end="")
     time.sleep(1)
     for i in range(5):
@@ -117,6 +127,7 @@ while new_game == "y":
     card_drawn = ai_hand.draw(current_deck)
     print("Computer drew a card.")
   
+  """Computer show hand"""
   print(f"Computer's hand:{ai_hand}. Hand's value: {ai_hand.value}")
 
   if player_hand.value > 21 and ai_hand.value > 21:
